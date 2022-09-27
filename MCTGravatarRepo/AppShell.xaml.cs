0bcc077a1b40719281410a3e67e269190e6f4049
Kym Phillpotts <kphillpotts@gmail.com>
2022-09-27 12:54:57 +1000

    Add project files.

---
+++ MCTGravatarRepo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MCTGravatarRepo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MCTGravatarRepo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MCTGravatarRepo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
